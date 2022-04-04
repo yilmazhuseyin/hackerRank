8adb44f4cde8edcd24da86405a6ba1bc8b2ba9b4
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-04-04 08:14:02 +0300

    Create CamelCase.cs

---
+++ C#/Prepare/Problem Solving/Strings/CamelCase.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(string s)
+    {
+        var count = 1;
+        foreach (char letter in s)
+        {
+            if (Convert.ToInt32(letter) >= 65 && Convert.ToInt32(letter) <= 90)
+                count++;
+        }
+        return count;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.camelcase(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
