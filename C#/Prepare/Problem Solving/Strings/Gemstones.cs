0e2795a1f40ad4104635a912cd7706b659ef48b8
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-04-15 09:04:14 +0300

    Create Gemstones.cs

---
+++ C#/Prepare/Problem Solving/Strings/Gemstones.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gemstones' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY arr as parameter.
+     */
+
+    public static int gemstones(List<string> arr)
+    {
+        var count = 0;
+        for (int i = 97; i <= 122; i++)
+        {
+            if (arr.All(x => XsdDataContractExporter.Contains(((char)(i)).ToString())))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> arr = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string arrItem = Console.ReadLine();
+            arr.Add(arrItem);
+        }
+
+        int result = Result.gemstones(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
