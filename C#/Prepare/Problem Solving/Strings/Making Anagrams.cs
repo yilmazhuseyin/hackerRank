91f5634ba80ef8414c19bbdbdd0da1b8dc65be99
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-04-22 08:29:50 +0300

    Create Making Anagrams.cs

---
+++ C#/Prepare/Problem Solving/Strings/Making Anagrams.cs
@@ -0,0 +1,64 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(string s1, string s2)
+    {
+        int count = 0;
+        String x = s2;
+
+        for (int i = 0; i < s1.Length; i++)
+        {
+            int index = x.IndexOf(s1.ElementAt(i));
+            if (index != -1)
+            {
+                x = x.Substring(0, index) + x.Substring(index + 1);
+                count++;
+            }
+        }
+
+        return s1.Length + s2.Length - count * 2;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
