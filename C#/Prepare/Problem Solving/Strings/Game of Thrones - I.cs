781e64b06f2cc475ef1382da61e2371fbd89eeb5
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-04-24 15:38:57 +0300

    Create Game of Thrones - I.cs

---
+++ C#/Prepare/Problem Solving/Strings/Game of Thrones - I.cs
@@ -0,0 +1,54 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gameOfThrones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string gameOfThrones(string s)
+    {
+        var abc = s.GroupBy(x => x).Select(x => new{Character = x.Key,Count = x.Count()}).ToList();
+        var tek = abc.Where(x => x.Count % 2 != 0).Count();
+        var cift = abc.Where(x => x.Count % 2 == 0).Count();
+        if (cift > 0 && tek <= 1)
+        {
+            return "YES";
+        }
+        return "NO";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.gameOfThrones(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
