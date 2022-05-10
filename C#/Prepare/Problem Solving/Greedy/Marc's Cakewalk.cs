88434e9cff83408261d9fd291b0f23759c1caa62
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-05-10 08:33:27 +0300

    Create Marc's Cakewalk.cs

---
+++ C#/Prepare/Problem Solving/Greedy/Marc's Cakewalk.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<int> calorie)
+    {
+        var list = calorie.OrderByDescending(x => x).ToList();
+        double total = 0;
+
+        for (int i = 0; i < list.Count; i++)
+            total += (Math.Pow(2,i) * list[i]);
+
+        return (Int64)total;
+    }   
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> calorie = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(calorieTemp => Convert.ToInt32(calorieTemp)).ToList();
+
+        long result = Result.marcsCakewalk(calorie);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
