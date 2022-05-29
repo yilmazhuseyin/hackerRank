2fc281e44a795285041b0b41f6154d63ae953eca
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-05-29 21:22:09 +0300

    Create Misère Nim.cs

---
+++ C#/Prepare/Problem Solving/Game Theory/Misère Nim.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'misereNim' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY s as parameter.
+     */
+
+    public static string misereNim(List<int> s)
+    {
+        int n = s.Count;
+        int xr = 0;
+        int sum = 0;
+
+        for(int i=0;i<n;i++){
+            xr ^= s[i];
+            sum += s[i];
+        }
+
+        if(n % 2 == 0)
+            return n != sum && xr == 0 ? "Second" : "First";
+        else
+            return n == sum || xr == 0 ? "Second" : "First";
+    
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+            string result = Result.misereNim(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
