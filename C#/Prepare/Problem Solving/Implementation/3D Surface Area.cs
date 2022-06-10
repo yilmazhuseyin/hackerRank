b2c9515c1d32412ddfb716c6cf2d5b78154bc29e
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-06-10 16:04:57 +0300

    Create 3D Surface Area.cs

---
+++ C#/Prepare/Problem Solving/Implementation/3D Surface Area.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'surfaceArea' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY A as parameter.
+     */
+
+    public static int surfaceArea(List<List<int>> A)
+{
+    int sum = 0;
+    for (int i = 0; i < A.Count; i++)
+        for (int j = 0; j < A[0].Count; j++)
+        {
+            sum += A[i][j] * 4 + 2;
+            if (j + 1 < A[0].Count)
+                sum -= 2 * Math.Min(A[i][j], A[i][j + 1]);
+            if (i + 1 < A.Count)
+                sum -= 2 * Math.Min(A[i][j], A[i + 1][j]);
+        }
+    return sum;
+}
+
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int H = Convert.ToInt32(firstMultipleInput[0]);
+
+        int W = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> A = new List<List<int>>();
+
+        for (int i = 0; i < H; i++)
+        {
+            A.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(ATemp => Convert.ToInt32(ATemp)).ToList());
+        }
+
+        int result = Result.surfaceArea(A);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
